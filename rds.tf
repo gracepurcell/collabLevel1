48bda1e84673405fead4169e4f84d2f467dd2626
tassianna <tasi.dafn@gmail.com>
2024-09-24 10:36:21 +0200

    rds resource

---
+++ rds.tf
@@ -0,0 +1,33 @@
+resource "aws_db_instance" "my_rds" {
+  allocated_storage    = 10
+  db_name              = "collabdb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = var.db-username
+  password             = var.db-password
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+
+  tags = {
+    Name = "Collab RDS"
+  }
+  
+}
+
+resource "aws_security_group" "db_sec_group" {
+  name        = "allow_mySQL"
+  description = "Allow MySQL"
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
